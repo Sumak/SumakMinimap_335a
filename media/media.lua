b8b0766ef921d7815c75a864ef23132867673644
Sumak <sumak.serge.m@gmail.com>
2015-06-07 19:19:45 +0300

    [1.0R005] Изменение путей [ 07.06.2015]

--- media/media.lua
+++ media/media.lua
@@ -3,31 +3,28 @@
 ---------------------------------------------
 local FCV, CFG, LOC = unpack(select(2, ...))
 
+mediapath = "Interface\\AddOns\\SumakMinimap\\media\\"
+
 CFG["media"] = {
 	---- fonts
-	["fontsize"] = 12, 														-- Size of font 
-	["font"] = [=[Interface\AddOns\SumakMinimap\media\fonts\ARIALN.TTF]=], -- основной шрифт
-	["uffont"] = [[Interface\AddOns\SumakMinimap\media\fonts\uf_font.ttf]], -- general font of unitframes
-	["dmgfont"] = [[Interface\AddOns\SumakMinimap\media\fonts\combat_font.ttf]], -- general font of dmg / sct
+	["fontsize"] = 12, 									    -- Размер шрифта 
+	["font"] = mediapath..[=[fonts\ARIALN.TTF]=], 			-- основной шрифт
+	["uffont"] = mediapath..[=[fonts\uf_font.ttf]=],		-- шрифт юнитфреймов
+	["dmgfont"] = mediapath..[=[fonts\combat_font.ttf]=], 	-- general font of dmg / sct
 	
 	---- textures
-	["normTex"] = [[Interface\AddOns\SumakMinimap\media\img\normTex]], -- texture used for tukui healthbar/powerbar/etc
-	["glowTex"] = [[Interface\AddOns\SumakMinimap\media\img\glowTex]], -- the glow text around some frame.
-	["bubbleTex"] = [[Interface\AddOns\SumakMinimap\media\img\bubbleTex]], -- unitframes combo points
-	["blank"] = [[Interface\AddOns\SumakMinimap\media\img\blank]], -- the main texture for all borders/panels
+	["normTex"] = mediapath..[=[img\normTex]=], 	-- texture used for tukui healthbar/powerbar/etc
+	["glowTex"] = mediapath..[=[img\glowTex]=], 	-- the glow text around some frame.
+	["bubbleTex"] = mediapath..[=[img\bubbleTex]=], -- unitframes combo points
+	["blank"] = mediapath..[=[img\blank]=],			-- the main texture for all borders/panels
 --  ["blank"] = [[Interface\\Buttons\\WHITE8x8]],
 	["bordercolor"] = { .3,.3,.3,1 }, -- border color of tukui panels
 	["altbordercolor"] = { .4,.4,.4,1 }, -- alternative border color, mainly for unitframes text panels.
 	["backdropcolor"] = { .1,.1,.1,1 }, -- background color of tukui panels
-	["buttonhover"] = [[Interface\AddOns\Tukui\media\img\button_hover]],
-	--["bgfile"] =  [[Interface\ChatFrame\ChatFrameBackground]],
-	["bgfile"] =[[Interface\ChatFrame\ChatFrameBackground]],
-    ["texture"] = [[Interface\\Addons\\SumakMinimap\\media\\img\\statusbar.tga]],
-	
-	---- sound
-	["whisper"] = [[Interface\AddOns\SumakMinimap\media\sounds\whisper.mp3]],
-	["warning"] = [[Interface\AddOns\SumakMinimap\media\sounds\warning.mp3]],
-	
-	["minimaparrow"] = [[Interface\Addons\SumakMinimap\media\img\MinimapArrow.blp]],
-	["mailicon"] = [[Interface\AddOns\SumakMinimap\media\img\mail]],
+	["buttonhover"] = mediapath..[=[img\button_hover]],
+	--["bgfile"] = [=[Interface\ChatFrame\ChatFrameBackground]=],
+	["bgfile"] =[=[Interface\ChatFrame\ChatFrameBackground]=],
+    ["texture"] = mediapath..[=[img\statusbar.tga]=],
+	["minimaparrow"] = mediapath..[=[img\MinimapArrow.blp]=],
+	["mailicon"] = mediapath..[=[img\mail.tga]=],
 }
