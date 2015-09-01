941e0454cb5e3706a141cf5be7e305e55ba08477
Sumak <sumak.serge.m@gmail.com>
2015-09-01 12:12:07 +0300

    [1.0R038]Добавление LeaveParty

--- SumakCore/core/slash.lua
+++ SumakCore/core/slash.lua
@@ -6,3 +6,13 @@ SlashCmdList.RELOADUI = ReloadUI
 SLASH_RELOADUI1 = "/rl"
 SLASH_RELOADUI2 = "/кд"
 
+----исправление невыхода из группы 
+-------------------------------------------------
+local PartyBye = function () 
+    LeaveParty()
+end
+
+SlashCmdList.Leave_Party = PartyBye
+
+SLASH_Leave_Party1 = "/lp"
+SLASH_Leave_Party2  = "/дз"
