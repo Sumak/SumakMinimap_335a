d99a84399936e67449bd11d43047d12f39b2b292
Sumak <sumak.serge.m@gmail.com>
2015-09-24 08:55:45 +0300

    [1.0R039]Изменение ChatAlert

--- SumakCore/core/chatalert.lua
+++ SumakCore/core/chatalert.lua
@@ -13,15 +13,18 @@ local addon_loaded_color = "|cff00ff00"
 local project_link = [=[ Страница проекта - [http://forum.wowcircle.com/showthread.php?t=455070] ]=]
 	
 FCV.PrintChatCommonInfo = function ()
+	print ("-------------------------------------")
 	print ("Аддон SumakUI специально для WOWCIRLE")
-	print ("Текущая версия - ".. addon_version)
+	print (format ("%q %s", "Текущая версия - ", addon_version_color .. "v." .. addon_version .. "|r"))
 	print (project_link)
 	print ("Спасибо за использование аддона")
+	print ("-------------------------------------")
 		
 end
 
 FCV.ChatAlert = function (string)
-	print (string)
+	str = format ("%s", addon_name_color .. string .. "|r")
+	print (str)
 end
 
 FCV.PrintAddonInfo = function (addon_name)
@@ -29,7 +32,7 @@ FCV.PrintAddonInfo = function (addon_name)
 	mem_usd = GetAddOnMemoryUsage(addon_name)
 	mem = format("%.2f", mem_usd).." kB"
 	addon_version = GetAddOnMetadata(addon_name, "Version")
-	print (format("%s %s %s", addon_name_color .. addon_name .. "|r", addon_version_color .. "v." .. addon_version .. "|r", addon_loaded_color .. "- загружен|r"));
+	print (format("%s %s", addon_name_color .. addon_name .. "|r", addon_loaded_color .. "- загружен|r"));
 	print (format("%s %s", addon_name_color .. "Использование памяти:|r", addon_version_color .. (mem or '') .. "|r"));
 end
 
